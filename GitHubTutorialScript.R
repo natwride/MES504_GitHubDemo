cc5d93ec39fbe8c425551720167361260ab8ea53
natwride <natwride@yahoo.com>
2026-04-15 11:37:52 -0400

    Filled out script exercise to demo github.

--- GitHubTutorialScript.R
+++ GitHubTutorialScript.R
@@ -1,5 +1,5 @@
-#INSERT YOUR NAME HERE
-#INSERT DATE HERE
+#INSERT YOUR NAME HERE: Natalie Wride
+#INSERT DATE HERE: 4/15/26
 
 #clear my workspace
 rm(list = ls())
@@ -11,10 +11,11 @@ library(learnr)
 #Generate random praise
 random_praise()
 #ADD COMMENT WITH YOUR RANDOM PRAISE PHRASE BELOW:
+#Absolutely fabulous!
 
 #Generate random encouragement
 random_encouragement()
 #ADD COMMENT WITH YOUR RANDOM ENCOURAGEMENT PHRASE BELOW:
-
+# Try it again. Perseverance is the key to success
 
 # commit and push your file to the GitHub repository
